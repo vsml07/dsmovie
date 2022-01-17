8b94347b5deb00c5aa078aa92f1469afb7f64f6e
vsml07 <vinicius.sml@gmail.com>
2022-01-17 13:14:40 -0300

    update nametag

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ function Navbar() {
                 <a href="https://github.com/vsml07" target="_blank" rel="noreferrer">
                     <div className="dsmovie-contact-container">
                         <GithubIcon />
-                        <p className="dsmovie-contact-link">/devsuperior</p>
+                        <p className="dsmovie-contact-link">/vsanches</p>
                     </div>
                 </a>
             </div>
